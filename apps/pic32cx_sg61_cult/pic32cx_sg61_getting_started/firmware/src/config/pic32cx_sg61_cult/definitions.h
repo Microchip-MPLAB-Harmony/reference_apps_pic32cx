4f9666b54d350193ecc3e8ff5b325bad4111c761
Sourav S Pasarad <sourav.spasarad@microchip.com>
2025-02-06 10:14:56 +0530

    [APP] Regenerated and tested the Getting Started on PIC32CX SG61 Cult
    M32MM-2953
    
    Signed-off-by: Sourav S Pasarad <sourav.spasarad@microchip.com>

--- apps/pic32cx_sg61_cult/pic32cx_sg61_getting_started/firmware/src/config/pic32cx_sg61_cult/definitions.h
+++ apps/pic32cx_sg61_cult/pic32cx_sg61_getting_started/firmware/src/config/pic32cx_sg61_cult/definitions.h
@@ -70,13 +70,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CX1025SG61128"
-#define DEVICE_ARCH			 "CORTEX-M4"
-#define DEVICE_FAMILY		 "PIC32CX"
-#define DEVICE_SERIES		 "PIC32CXSG61"
+#define DEVICE_NAME          "PIC32CX1025SG61128"
+#define DEVICE_ARCH          "CORTEX-M4"
+#define DEVICE_FAMILY        "PIC32CX"
+#define DEVICE_SERIES        "PIC32CXSG61"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 120000000
+#define CPU_CLOCK_FREQUENCY 120000000U
 
 // *****************************************************************************
 // *****************************************************************************
